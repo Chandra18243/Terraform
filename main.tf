2e4759567db2bb07b3dd1d077872a6b1e7dbcb76
root <root@ip-172-31-15-218.ap-south-1.compute.internal>
2025-07-29 18:16:22 +0000

    Initial commit of Terraform project

---
+++ main.tf
@@ -0,0 +1,12 @@
+resource "aws_instance" "one" {
+  ami               = var.ami_id
+  instance_type     = var.itype
+  key_name          = var.kname
+  availability_zone = var.azone
+
+  tags = {
+    Name        = var.iname
+    Environment = var.ename
+  }
+}
+
---
+++ variables.tf
@@ -0,0 +1,30 @@
+variable "ami_id" {
+  type    = string
+  default = "ami-0f918f7e67a3323f0"
+}
+
+variable "itype" {
+  type    = string
+  default = "t2.micro"
+}
+
+variable "iname" {
+  type    = string
+  default = "new_instance"
+}
+
+variable "kname" {
+  type    = string
+  default = "chandra_Devops"
+}
+
+variable "azone" {
+  type    = string
+  default = "ap-south-1a"
+}
+
+variable "ename" {
+  type    = string
+  default = "dev"
+}
+
